4ae9b58e5d778e8c43eeb30da76b2f25c0161947
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2024-12-01 06:54:06 +0200

    create tunes item

---
+++ lib/widgets/tunes_item.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TunesItem extends StatelessWidget {
+  const TunesItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {},
+        child: Container(
+          
+        ),
+      ),
+    );
+  }
+}
