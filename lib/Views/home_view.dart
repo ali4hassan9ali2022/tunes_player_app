1398af549c9ff423ec5ebd223873c244955b31ab
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2024-12-01 07:00:03 +0200

    finish app

--- lib/Views/home_view.dart
+++ lib/Views/home_view.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:tunes_player_app/Models/item_model.dart';
+import 'package:tunes_player_app/widgets/tunes_item.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
-
+  final List<ItemModel> item = const [
+    ItemModel(color: Color(0xffF44336), sound: 'note1.wav'),
+    ItemModel(color: Color(0xffF89800), sound: 'note2.wav'),
+    ItemModel(color: Color(0xffFEEB3B), sound: 'note3.wav'),
+    ItemModel(color: Color(0xff4CAF50), sound: 'note4.wav'),
+    ItemModel(color: Color(0xff2F9688), sound: 'note5.wav'),
+    ItemModel(color: Color(0xff2896F3), sound: 'note6.wav'),
+    ItemModel(color: Color(0xff9C27B0), sound: 'note7.wav'),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +24,13 @@ class HomeView extends StatelessWidget {
         ),
         backgroundColor: const Color(0xff253238),
         elevation: 0,
+      ),
+      body: Column(
+        children: item
+            .map(
+              (e) => TunesItem(item: e),
+            )
+            .toList(),
       ),
     );
   }
--- lib/widgets/tunes_item.dart
+++ lib/widgets/tunes_item.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:tunes_player_app/Models/item_model.dart';
 
 class TunesItem extends StatelessWidget {
-  const TunesItem({super.key});
-
+  const TunesItem({super.key, required this.item});
+  final ItemModel item;
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: GestureDetector(
-        onTap: () {},
+        onTap: () {
+          item.playSound();
+        },
         child: Container(
-          
+          color: item.color,
         ),
       ),
     );
