bc6d825cb613e5558fff7068e88a51107797e1cb
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2024-12-01 06:56:43 +0200

    create item model

---
+++ lib/Models/item_model.dart
@@ -0,0 +1,12 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+class ItemModel {
+  final Color color;
+  final String sound;
+  const ItemModel({required this.color, required this.sound});
+  playSound() {
+    final player = AudioPlayer();
+    player.play(AssetSource(sound));
+  }
+}
